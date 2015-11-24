328967ecc5d3051a72b0a4ca843ca70d5d8d6827
GP Orcullo <kinsamanka@gmail.com>
2015-11-24 21:45:31 +0700

    cleanups

---
+++ install_wheezy_deps.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -ex
+
+# add dovetail gpg key
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-key 73571BB9
+
+# update apt sources
+echo "deb http://deb.dovetail-automata.com wheezy main" \
+	> /etc/apt/sources.list.d/machinekit.list
+apt-get update
+
+# install requisite packages
+apt-get install -y --no-install-recommends \
+	git \
+	devscripts \
+	fakeroot \
+	equivs \
+	lsb-release \
+	less
+
+# install machinekit build depends
+apt-get install -y -t wheezy-backports cython
+
+deps=`cat mk_depends`
+deps="${deps%
+}"
+
+apt-get install -y --no-install-recommends ${deps}
